46459c452e2736faaf95cc618726b16d65be54b4
ShanghaiJieGuKeJi <dh@ifourthwall.com>
2024-03-05 17:52:57 +0800

    异步加载

---
+++ lazy-load/src/lazyLoad.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- Use defineAsyncComponent  -->
+    <button @click="show = true">Login</button>
+    <login-popup v-if="show" />
+  </template>
+  
+  <script>
+  import { defineAsyncComponent } from 'vue'
+  export default {
+    components: {
+      LoginPopup: defineAsyncComponent(() =>
+        import('./components/LoginPopup.vue')
+        // import("http://devcrate.local/product/components/Subcomponent.vue")
+      ),
+    },
+    data() {
+      return {
+        show: false,
+      }
+    },
+  }
+  </script>
+  
---
+++ lazy-load/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './lazyLoad.vue'
+
+createApp(App).mount('#app')
